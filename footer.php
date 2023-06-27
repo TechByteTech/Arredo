003f6b3e8540dcd44a709cc64329d450821673f9
Tushar <Tusharmathur2206@gmail.com>
2023-06-27 22:17:37 +0530

    footerfile

---
+++ footer.php
@@ -0,0 +1,135 @@
+<style>
+      .social-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.social-icons a {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin: 10px;
+  color: #fff;
+  text-align: center;
+  line-height: 50px;
+  font-size: 30px;
+  transition: all 0.3s ease-in-out;
+}
+
+.social-icons a.facebook {
+  background-color: #3b5998;
+}
+
+.social-icons a.whatsapp {
+  background-color: #25D366;
+}
+.social-icons a.youtube {
+  background-color: red;
+}
+
+.social-icons a.instagram {
+  background-color: #e4405f;
+}
+
+.social-icons a:hover {
+  transform: scale(1.2);
+}
+
+
+
+
+
+      </style>
+
+
+
+    <section class="">
+  <!-- Footer -->
+  <footer class="bg-secondary text-white text-center">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+
+      
+      <img src="photos/arredo-logo.png" style="object-fit:cover;height:80px;width:100px;">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0" style="width:500px;">
+          <h5 class="text-uppercase">Connects us on</h5>
+
+          
+    <div id="social-media-contact" style="height:80px;">
+      <div style="text-align:center;font-weight:bold;color:Black;">
+      Footer Content
+      </div>
+
+      <div class="social-icons">
+        <a href="https://m.facebook.com/100528986236131?wtsid=rdr_08O0igl8BJDHXDgur" class="facebook" ><i class="fa fa-facebook"></i></a>
+        <a href="#" class="instagram" ><i class="fa fa-instagram"></i></a>
+        <a href="https://wa.me/919810410995" class="whatsapp" ><i class="fa fa-whatsapp"></i></a>
+        <a href="https://wa.me/919810410995" class="youtube" ><i class="fa fa-youtube"></i></a>
+</div>
+
+
+  <p style="color:black;">address:bharati vidyapeeth college of engg.</p>
+    </div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Links</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
